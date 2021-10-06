b4e62aa384532bdf80f828e25743a7839f114559
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-06 07:03:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,3 +7,19 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+Animes =Cient(
+  'simp',
+  api_id=Config.API_ID,
+  api_hash=Config.API_HASH,
+  bot_token=Config.BOT_TOKEN
+)
+
+@Animes.on_message(filters.command("start"))
+def start(Animes, msg):
+  Animes.send_message("Hi there")
+  
+@Animes.on_message(filters.command("id"))
+def get_id(Animes, mag):
+  msg.reply(f"Your id is {msg.from_user.id} \nJoin @Animes_Encoded")
+  
